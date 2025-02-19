646418c8e5853e6fe552f3c7b6548be6b3afc0b2
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-19 12:45:37 +0600

    device class

---
+++ smartphone.py
@@ -0,0 +1,17 @@
+from device import Device
+
+class Smartphone(Device):
+    def __init__(self, name, price, stock, warranty_period, screen_size, battery_life):
+        super().__init__(name, price, stock, warranty_period)
+        self.screen_size = screen_size
+        self.battery_life = battery_life
+
+    def display_info(self):
+        super().display_info()
+        print(f"Screen Size: {self.screen_size} inches, Battery Life: {self.battery_life} hours")
+
+    def make_call(self):
+        print(f"Making a call from {self.name}")
+
+    def install_app(self):
+        print(f"Installing an app on {self.name}")
