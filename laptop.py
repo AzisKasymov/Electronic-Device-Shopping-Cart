b0d0a943865f84e4403e5589e7cbfab7ed169fb0
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-19 13:53:09 +0600

    laptop class

--- laptop.py
+++ laptop.py
@@ -6,3 +6,12 @@ class Laptop(Device):
         self.ram_size = ram_size
         self.processor_speed = processor_speed
 
+    def display_info(self):
+        super().display_info()
+        print(f"RAM: {self.ram_size}GB, Processor: {self.processor_speed}GHz")
+
+    def run_program(self):
+        print(f"Running a program o {self.name}")
+
+    def use_keyboard(self):
+        print(f"Typing on {self.name}")
