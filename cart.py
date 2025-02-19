e375e9ac20757cc7a36c02547c7dceadaa6de5c1
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-19 15:16:34 +0600

    cart class

--- cart.py
+++ cart.py
@@ -8,14 +8,22 @@ class Cart:
         if device.is_available(amount):
             self.items.append((device, amount))
             device.reduce_stock(amount)
-            print(f"Added {amount} {device.name} to cart.")
+            print(f"added {amount} {device.name} to cart.")
         else:
-            print(f"Not enough stock for {device.name}.")
+            print(f"not enough stock for {device.name}.")
 
     def remove_device(self, device, amount):
         for item in self.items:
             if item[0] == device and item[1] >= amount:
                 self.items.remove(item)
                 device.stock += amount
-                print(f"Removed {amount} {device.name} from cart.")
+                print(f"removed {amount} {device.name} from cart.")
                 break
+    def calculate_total(self):
+        return sum(item[0].price * item[1] for item in self.items)
+
+    def checkout(self):
+        total = self.calculate_total()
+        print(f"Total amount: ${total:.2f}")
+        self.items.clear()
+        print("checkout completed,thank!")
