e1fb318f2f15674ae7224e2ca6c4cfb33f118bf5
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-19 12:33:07 +0600

    device class

---
+++ device.py
@@ -0,0 +1,21 @@
+class Device:
+    def __init__(self, name, price, stock, warranty_period):
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.warranty_period = warranty_period
+
+    def display_info(self):
+        print(f"Name: {self.name}, Price: ${self.price}, Stock: {self.stock}, Warranty: {self.warranty_period} months")
+
+    def apply_discount(self, discount_percentage):
+        self.price -= self.price * (discount_percentage / 100)
+
+    def is_available(self, amount):
+        return self.stock >= amount
+
+    def reduce_stock(self, amount):
+        if self.is_available(amount):
+            self.stock -= amount
+            return True
+        return False
