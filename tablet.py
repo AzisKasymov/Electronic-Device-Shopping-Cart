60c92a63bd936e21a76c00607313b9efed0a3311
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-19 14:34:45 +0600

    tablet class

--- tablet.py
+++ tablet.py
@@ -9,3 +9,9 @@ class Tablet(Device):
     def display_info(self):
         super().display_info()
         print(f"Resolution: {self.screen_resolution}, Weight: {self.weight}g")
+
+    def  browse_internet (self):
+        print(f"Browsing the internet on {self.name}")
+
+    def use_touchscreen(self):
+        print(f"using touchscreen on {self.name}")
