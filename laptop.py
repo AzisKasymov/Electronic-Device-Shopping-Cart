3302770c03a50fa7835d108dbc4b740abaea9d64
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-19 13:50:24 +0600

    laptop class

---
+++ laptop.py
@@ -0,0 +1,8 @@
+from device import Device
+
+class Laptop(Device):
+    def __init__(self, name, price, stock, warranty_period, ram_size, processor_speed):
+        super().__init__(name, price, stock, warranty_period)
+        self.ram_size = ram_size
+        self.processor_speed = processor_speed
+
